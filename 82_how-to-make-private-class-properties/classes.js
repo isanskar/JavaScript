da2b1c065f0d99bcb3163aefe30e74c3a252036f
sanskar <sanskarshrivastava31103@gmail.com>
2025-11-20 23:26:40 +0530

    private field

---
+++ 82_how-to-make-private-class-properties/classes.js
@@ -0,0 +1,28 @@
+class CreateUser {
+    #age
+    constructor(firstName, lastName, age) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.#age = age
+    }
+
+    #hi = 'hello world'
+
+    getBirthYear(){
+        console.log(this.#hi)
+        console.log(this.#getFullName())
+        return new Date().getFullYear() - this.#age
+    }
+
+    #getFullName() {
+        return this.firstName + ' ' + this.lastName
+    }
+}
+
+const user1 = new CreateUser('Aman', 'Mishra', 32)
+const user2 = new CreateUser('Anurag', 'Singh', 72)
+
+
+// console.log(user1.#age)
+
+
