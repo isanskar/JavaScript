34f329e84affd2765d95ebcca436a128efe104cf
sanskar <sanskarshrivastava31103@gmail.com>
2025-11-21 17:01:12 +0530

    getter setter

---
+++ 84_getters-and-setters/script.js
@@ -0,0 +1,43 @@
+class CreateUser {
+    constructor(firstName, lastName, age) {
+        this.firstName = firstName,
+        this.lastName = lastName,
+        this.age = age
+    }
+
+    getBirthYear(){
+        return new Date().getFullYear() - this.age
+    }
+
+    get fullName() {
+        return `${this.firstName} ${this.lastName}`
+    }
+
+    set fullName(value){
+        const [firstName,lastName] = value.split(' ');
+        this.firstName = firstName
+        this.lastName = lastName
+    }
+}
+
+const user1 = new CreateUser('Aman', 'Mishra', 32)
+const user2 = new CreateUser('Anurag', 'Singh', 72)
+
+
+// const user = {
+//     firstName : 'Anurag',
+//     lastName : 'Singh',
+//     get fullName(){
+//         console.log('getting....')
+//         return `${user.firstName} ${user.lastName}`
+//     },
+
+//     set fullName(value){
+//         console.log('setting...')
+//         const [firstName,lastName] = value.split(' ');
+//         this.firstName = firstName
+//         this.lastName = lastName
+//     }
+// }
+
+// console.log(user.fullName)
